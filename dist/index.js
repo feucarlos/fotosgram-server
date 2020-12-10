455738e4c11e608c308a404f621bdb6b6d771b08
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2020-12-09 18:11:27 -0600

    conexión a DB

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
+const usuarios_1 = __importDefault(require("./routes/usuarios"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
+// Rutas de la app
+server.app.use("/user", usuarios_1.default);
+// Conectar DB
+mongoose_1.default.connect("mongodb://localhost:27017/fotosgram", {
+    useNewUrlParser: true, useCreateIndex: true
+}, (err) => {
+    if (err)
+        throw err;
+    console.log("Base de datos ONLINE");
+});
 // Levantar express
 server.start(() => {
     console.log(`Servidor corriendo en puerto ${server.port}`);
--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
 import Server from "./classes/server";
+import userRoutes from "./routes/usuarios";
+import mongoose from "mongoose";
 
 const server = new Server();
+
+
+// Rutas de la app
+server.app.use("/user", userRoutes);
+
+// Conectar DB
+mongoose.connect("mongodb://localhost:27017/fotosgram", {
+    useNewUrlParser: true, useCreateIndex: true
+}, (err) => {
+    if (err) throw err;
+    console.log("Base de datos ONLINE");
+    
+})
+
+
 
 // Levantar express
 server.start( ()=> {
